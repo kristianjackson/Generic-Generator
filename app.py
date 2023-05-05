6061df16dc9863cda4caceeca2376e2fec9d7ef9
Kristian Jackson <kristian.jackson@gmail.com>
2023-05-05 09:24:37 -0400

    Update app.py
    environment variables are set in the environment not in the files now

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Bring in deps
 import os 
-from apikey import apikey 
+# from apikey import apikey 
 
 import streamlit as st 
 from langchain.llms import OpenAI
@@ -9,7 +9,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+# os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
 st.title('🦜🔗 YouTube GPT Creator')
