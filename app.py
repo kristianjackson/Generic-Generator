1a1ed174aaf89a41f5fc5762f4caa5d34e0fe0c4
Kristian Jackson <kristian.jackson@gmail.com>
2023-05-05 14:33:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ title_template = PromptTemplate(
 
 exec_summary_template = PromptTemplate(
     input_variables = ['title', 'wikipedia_research'], 
-    template='write me executive summary paragraph based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
+    template='write me executive summary paragraph based on this title: {title} while leveraging this wikipedia research: {wikipedia_research} '
 )
 
 introduction_template = PromptTemplate(
     input_variables = ['title', 'wikipedia_research', 'exec_summary'], 
-    template='write me introduction paragrapgh for the white paper based on this title: {title} while leveraging this wikipedia research: {wikipedia_research} and this executive paragraph: {exec_summary} '
+    template='write me introduction paragrapgh for a white paper based on this title: {title} while leveraging this wikipedia research: {wikipedia_research} and this executive paragraph: {exec_summary} '
 )
 
 # Memory 
@@ -41,8 +41,8 @@ introduction_memory = ConversationBufferMemory(input_key='exec_summary', memory_
 # Llms
 llm = OpenAI(temperature=TEMPERATURE) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
-exec_summary_chain = LLMChain(llm=llm, prompt=exec_summary_template, verbose=True, output_key='script', memory=exec_summary_memory)
-introduction_chain = LLMChain(llm=llm, prompt=introduction_template, verbose=True, output_key='script', memory=introduction_memory)
+exec_summary_chain = LLMChain(llm=llm, prompt=exec_summary_template, verbose=True, output_key='exec_summary', memory=exec_summary_memory)
+introduction_chain = LLMChain(llm=llm, prompt=introduction_template, verbose=True, output_key='introduction', memory=introduction_memory)
 
 wiki = WikipediaAPIWrapper()
 
@@ -53,18 +53,8 @@ if prompt:
     exec_summary = exec_summary_chain.run(title=title, wikipedia_research=wiki_research)
     introduction = introduction_chain.run(title=title, wikipedia_research=wiki_research, exec_summary=exec_summary)
 
-    st.write(title) 
+    st.write(title)
+    st.write("Executive Summary")
     st.write(exec_summary)
+    st.write("Analysis")
     st.write(introduction)
-
-    with st.expander('Title History'): 
-        st.info(title_memory.buffer)
-
-    with st.expander('Executive Summary History'): 
-        st.info(exec_summary_memory.buffer)
-
-    with st.expander('Introduction Paragraph History'): 
-        st.info(introduction_memory.buffer)
-
-    with st.expander('Wikipedia Research'): 
-        st.info(wiki_research)
